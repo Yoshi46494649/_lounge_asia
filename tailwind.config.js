ed2e153294f3fac763f481321a060ed85b999f2d
Yoshi46494649 <yoshiyasu1304@gmail.com>
2026-04-01 11:54:20 +1000

    Auto backup: 2026-04-01 11:54:19

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
-﻿export default {
+export default {
   content: [
-    "./index.html",
-    "./brisbane.html",
-    "./speed-dating/**/*.{js,html}"
+    "./**/*.html",
+    "./speed-dating/**/*.{js,html}",
+    "./assets/**/*.js"
   ],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         sans: ['Outfit', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
       },
       colors: {
         brand: {
           dark: '#111827',
-          primary: '#10B981',
-          accent: '#FBBF24',
+          primary: '#D4AF37', // Champagne Gold
+          matcha: '#10B981',
+          accent: '#D4AF37',
         }
       }
     }
